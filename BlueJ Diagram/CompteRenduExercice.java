bd82c0c51c4db0f62028257a68be86c9a4c4c702
amimaresi <ma_hassam@esi.dz>
2024-05-17 18:41:25 +0100

    changed test

--- BlueJ Diagram/CompteRenduExercice.java
+++ BlueJ Diagram/CompteRenduExercice.java
@@ -2,14 +2,20 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class CompteRenduExercice implements CompteRendu{
-    HashMap<String,ArrayList<Integer>> Notes;
-    public float getNoteExo(String consigne){return 0;
+    HashMap<String,ArrayList<Integer>> notes;
+    public float getNoteExo(String consigne){
+        ArrayList<Integer> exo=notes.get(consigne);
+        float sum=0;
+        for (Integer note : exo) { 		      
+           sum+=note;
+      }
+      return sum/exo.size();
     }
     public float getNoteTotale(){return 0;}
-    public int getNoteTentative(String consigne,int index){
-        return Notes.get(consigne).size()>index ?Notes.get(consigne).get(index):-1;
+    public ArrayList<Integer> getNoteTentatives(String consigne,int index){
+        return notes.get(consigne);
     }
     public void setNoteTentative(String consigne,int index,int note){
-        Notes.get(consigne).add(note);
+        notes.get(consigne).add(note);
     }
 }
--- BlueJ Diagram/CompteRenduQuestion.java
+++ BlueJ Diagram/CompteRenduQuestion.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CompteRenduQuestion implements CompteRendu{
+    HashMap<String,Integer> notes;
+    public float getNoteQuestion(String consigne){
+        return notes.get(consigne);
+    }
+    
+    public float getNoteTotale(){
+        float sum=0; 
+        for (var note : notes.entrySet()) { 		      
+           sum+=note.getValue();		
+      }
+      return sum/notes.size();
+    }
+    public void setNoteQuestion(String consigne,int note){
+        notes.put(consigne,note);
+    }
 }
